d5b99eb4d18b4e7768dd34a789d1424e521facd6
Brian Philip <bphilip777@gmail.com>
2022-02-16 23:18:01 -0700

    Incorporated a lower memory algorithm where everything is performed within the same loop.

--- Scipts/AddTwoNumbers.py
+++ Scipts/AddTwoNumbers.py
@@ -4,36 +4,53 @@ class ListNode:
         self.nextNode = nextNode
 
 def addTwoNumbers(l1:ListNode, l2:ListNode) -> ListNode:
-    head = ListNode(0, None)
+    head = ListNode()
     if type(l1) is not ListNode or type(l2) is not ListNode:
         return head
     rollover = False
     current = head
-    while l1 is not None or l2 is not None:
-        val = 0
-        if l1 is not None:
-            val += l1.val
+    while l1 or l2:
+        if l1:
+            current.val += l1.val
             l1 = l1.nextNode
-        if l2 is not None:
-            val += l2.val
+        if l2:
+            current.val += l2.val
             l2 = l2.nextNode
         if rollover:  # Check if on the previous loop rollover was greater than 10
-            val += 1
+            current.val += 1
             rollover = False
-        if val > 9:
+        if current.val > 9:
             rollover = True
-            val -= 10
-        current.val = val
+            current.val -= 10
         # Prepare for next loop
-        if l1 is not None or l2 is not None:
-            current.nextNode = ListNode(0)
+        if l1 or l2:  # Are there more values?
+            current.nextNode = ListNode()
             current = current.nextNode
-        elif rollover:
-            current.nextNode = ListNode(1, None)
+        elif rollover:  # Was there a rollover?
+            current.nextNode = ListNode(1)
             break
-        else:
+        else:  # Exit
             break
     return head
+
+# Way less memory since everything is done in one loop instead
+def addTwoNumbersLowMemory(l1:ListNode, l2:ListNode) -> ListNode:
+    if not l1 or not l2:
+        return l1 or l2
+    head = ListNode()
+    cur = head
+    carry, val = 0, 0
+    while l1 or l2 or carry:
+        val = carry
+        val += l1.val if l1 else 0
+        val += l2.val if l2 else 0
+        carry = 1 if val > 9 else 0
+        val -= 10 if carry else 0
+        cur.nextNode = ListNode(val)
+        cur = cur.nextNode
+        l1 = l1.nextNode if l1 else None
+        l2 = l2.nextNode if l2 else None
+    return head.nextNode
 
 def convertListToListNode(values:list[int]) -> ListNode:
     # Initialize head of list node
@@ -60,8 +77,10 @@ l2T = [5, 6, 4]
 list1 = convertListToListNode(l1T)
 list2 = convertListToListNode(l2T)
 list3 = addTwoNumbers(list1, list2)
-printListNode(list3)
+list4 = addTwoNumbersLowMemory(list1, list2)
+printListNode(list4)
 
+'''
 # Check again
 l1T = [0]
 l2T = [0]
@@ -81,3 +100,4 @@ printListNode(list3)
 # Simply exchanging modulo for subtract improved performance
 # Simply swapping head rather than re-instancing it improved speed by so much!
 # Apparently doing head = current = Listnode is much slower than doing head = listnode; current = listnode - maybe the way they speed up the code?
+'''
